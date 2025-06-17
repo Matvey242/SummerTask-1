bc34273fd8a8db871c56a9b55ec12c959c884e71
Matvey <hicenkomatvej@gmail.com>
2025-06-17 18:18:25 +0300

    added chat controller

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,17 +1,11 @@
 import express from 'express'
 import { getAllUsers } from '../controllers/userController.js'
 import { authMiddleware } from '../middlewares/authMiddleware.js'
-import { createChat, getMyChats, getChatById, joinChat, createMessage, getChatMessages } from '../controllers/chatController.js'
 
 const router = express.Router()
 
 // get -> получение всех пользователей
 router.get('/', authMiddleware, getAllUsers)
-router.post('/createChat', authMiddleware, createChat)
-router.get('/getChats', authMiddleware, getMyChats)
-router.get('/getChatById', authMiddleware, getChatById)
-router.post('/joinChat', authMiddleware, joinChat)
-router.post('/Chat/:chatId', authMiddleware, createMessage)
-router.get('/Chat/:chatId', authMiddleware, getChatMessages)
+
 
 export default router
